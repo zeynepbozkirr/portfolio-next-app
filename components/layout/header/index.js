ab81bd7ecc33e3697a9b151d9e27d11f872a62c5
zeynepbozkirr <bozkrzeynep@gmail.com>
2023-01-10 23:57:02 +0300

    first commit

---
+++ components/layout/header/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./header.module.css";
+
+const HeaderComp = () => {
+  return <header className={styles.navbar}>head</header>;
+};
+
+export default HeaderComp;
